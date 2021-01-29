57a98a0fe389be8a24bb78c70b60667bfb1f0cae
msgchinh <vanchinhpc9.com>
2021-01-29 17:37:57 +0700

    create graphql mutation create rma, send message, confirm shipping

--- Model/Resolver/Rmas.php
+++ Model/Resolver/Rmas.php
@@ -7,11 +7,14 @@ declare(strict_types=1);
 
 namespace Lof\RmaGraphQl\Model\Resolver;
 
+use Magento\CustomerGraphQl\Model\Customer\GetCustomer;
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\GraphQl\Query\Resolver\Argument\SearchCriteria\Builder as SearchCriteriaBuilder;
+use Magento\GraphQl\Model\Query\ContextInterface;
 
 class Rmas implements ResolverInterface
 {
@@ -24,14 +27,21 @@ class Rmas implements ResolverInterface
      * @var DataProvider\Rma
      */
     private $rmaProvider;
+    /**
+     * @var GetCustomer
+     */
+    private $getCustomer;
 
     public function __construct(
         DataProvider\Rma $RmaRepository,
-        SearchCriteriaBuilder $searchCriteriaBuilder
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        GetCustomer $getCustomer
+
     )
     {
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->rmaProvider = $RmaRepository;
+        $this->getCustomer = $getCustomer;
     }
 
     /**
@@ -50,11 +60,18 @@ class Rmas implements ResolverInterface
         if ($args['pageSize'] < 1) {
             throw new GraphQlInputException(__('pageSize value must be greater than 0.'));
         }
+
+        if (!$context->getExtensionAttributes()->getIsCustomer()) {
+            throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
+        }
+
+        $customerId = $this->getCustomer->execute($context)->getId();
+
         $searchCriteria = $this->searchCriteriaBuilder->build( 'lof_rma_rma', $args );
         $searchCriteria->setCurrentPage( $args['currentPage'] );
         $searchCriteria->setPageSize( $args['pageSize'] );
 
-        $searchResult = $this->rmaProvider->getListRmas( $searchCriteria );
+        $searchResult = $this->rmaProvider->getListRmas( $searchCriteria , $customerId);
 
         return [
             'total_count' => $searchResult->getTotalCount(),
